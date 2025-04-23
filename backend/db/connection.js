242252c12ef78c02dda1f171cfc6abfac0cc3170
mengqiu97 <liu.mengqiu@northeastern.edu>
2025-04-23 01:29:46 -0700

    modified game logic

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb://localhost:27017/battleship",
+      process.env.MONGODB_URI || "mongodb://localhost:27017/battleship",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
